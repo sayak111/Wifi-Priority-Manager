cd3904ca2c7fc5d7985f761a1d7fb6bbc6ffe44d
your name <your@email.com>
2025-06-17 09:40:40 +0300

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WifiPriorityManager
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxProfiles = new ListBox();
+            txtPriority = new TextBox();
+            label1 = new Label();
+            btnSetPriority = new Button();
+            btnResetPriorities = new Button();
+            SuspendLayout();
+            // 
+            // listBoxProfiles
+            // 
+            listBoxProfiles.FormattingEnabled = true;
+            listBoxProfiles.ItemHeight = 15;
+            listBoxProfiles.Location = new Point(240, 52);
+            listBoxProfiles.Name = "listBoxProfiles";
+            listBoxProfiles.Size = new Size(120, 94);
+            listBoxProfiles.TabIndex = 0;
+            // 
+            // txtPriority
+            // 
+            txtPriority.Location = new Point(470, 94);
+            txtPriority.Name = "txtPriority";
+            txtPriority.Size = new Size(100, 23);
+            txtPriority.TabIndex = 1;
+            txtPriority.TextChanged += txtPriority_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(366, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(98, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Priority (1, 2, 3...):";
+            // 
+            // btnSetPriority
+            // 
+            btnSetPriority.Location = new Point(366, 123);
+            btnSetPriority.Name = "btnSetPriority";
+            btnSetPriority.Size = new Size(75, 23);
+            btnSetPriority.TabIndex = 3;
+            btnSetPriority.Text = "Set Priority";
+            btnSetPriority.UseVisualStyleBackColor = true;
+            btnSetPriority.Click += btnSetPriority_Click;
+            // 
+            // btnResetPriorities
+            // 
+            btnResetPriorities.Location = new Point(486, 123);
+            btnResetPriorities.Name = "btnResetPriorities";
+            btnResetPriorities.Size = new Size(75, 23);
+            btnResetPriorities.TabIndex = 4;
+            btnResetPriorities.Text = "Reset Priorities";
+            btnResetPriorities.UseVisualStyleBackColor = true;
+            btnResetPriorities.Click += btnResetPriorities_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnResetPriorities);
+            Controls.Add(btnSetPriority);
+            Controls.Add(label1);
+            Controls.Add(txtPriority);
+            Controls.Add(listBoxProfiles);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += MainForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBoxProfiles;
+        private TextBox txtPriority;
+        private Label label1;
+        private Button btnSetPriority;
+        private Button btnResetPriorities;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WifiPriorityManager
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetPriority_Click(object sender, EventArgs e)
+        {
+            if (listBoxProfiles.SelectedItem != null && int.TryParse(txtPriority.Text, out int priority))
+            {
+                string profile = listBoxProfiles.SelectedItem.ToString();
+                string cmd = $"wlan set profileorder name=\"{profile}\" interface=\"Wi-Fi\" priority={priority}";
+                RunNetshCommand(cmd);
+                MessageBox.Show($"Priority set for '{profile}' to {priority}");
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadWifiProfiles();
+        }
+
+        private void LoadWifiProfiles()
+        {
+            listBoxProfiles.Items.Clear();
+            var process = new Process();
+            process.StartInfo.FileName = "netsh";
+            process.StartInfo.Arguments = "wlan show profiles";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            foreach (var line in output.Split('\n'))
+            {
+                if (line.Trim().StartsWith("All User Profile"))
+                {
+                    string profile = line.Split(':')[1].Trim();
+                    listBoxProfiles.Items.Add(profile);
+                }
+            }
+        }
+
+        private void RunNetshCommand(string command)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "netsh";
+            process.StartInfo.Arguments = command;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void txtPriority_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResetPriorities_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBoxProfiles.Items.Count; i++)
+            {
+                string profile = listBoxProfiles.Items[i].ToString();
+                int priority = i + 1;
+
+                string cmd = $"wlan set profileorder name=\"{profile}\" interface=\"Wi-Fi\" priority={priority}";
+                RunNetshCommand(cmd);
+            }
+
+            MessageBox.Show("Priorities have been reset (1, 2, 3...)");
+        }
+
+    }
+}
